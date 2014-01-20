30d0c0e99ca699ebd87ee0f0086fc8afe1e8e3bc
WeihuaQiu <leo.qwh@gmail.com>
2014-01-20 11:26:02 +0800

    Revert "add a new file"
    This reverts commit f077deda656050bd6bc7075f90cabb280c9e24cd.

--- PE96/sudoku.cpp
+++ PE96/sudoku.cpp
@@ -1,29 +1,50 @@
-
-
-
-//using namespace std;
-void get_sudoku_matrix(int [][9][9]);
-bool calculate_answer(int [][9][9]);
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int calculate_answer(int [][9][9]);
 void solve_sudoku(int [][9]);
 
 void main()
 {
-	int i;
-
+	char sudoku_matrix[50][9][9]={0};
 	int s_m[50][9][9],s_m_solved[50][9][9];
-	
+	string s;
+	char a;
+	int i,j,k;
 
-	get_sudoku_matrix(s_m);
+	ifstream fin("sudoku.txt",ios::in);
+
+	if(!fin)
+	{
+		cout<<"File open error!"<<endl;
+		return;
+	}
 
 	for(i=0;i<50;i++)
 	{
-		solve_sudoku(s_m[i]);
+		getline(fin,s);	
+		for(j=0;j<9;j++)
+		{
+			fin.read(sudoku_matrix[i][j],9);
+			getline(fin,s);
+		}
 	}
 
-	//calculate_answer(s_m);
-	//solve_sudoku(s_m_solved[i]);
-	//calculate_answer(s_m_solved);
+	for(i=0;i<50;i++)
+	{	for(j=0;j<9;j++)
+		{	for(k=0;k<9;k++)
+			{	s_m[i][j][k] = (int)sudoku_matrix[i][j][k] - 48;
+				s_m_solved[i][j][k] = (int)sudoku_matrix[i][j][k] - 48;
 
-	//cout<<(int)sudoku_matrix[0][0][0]<<endl;
-	//cout<<(int)sudoku_matrix[1][1][1]<<endl;
+				solve_sudoku(s_m_solved[i]);
+			}
+		}
+	}
+
+
+	calculate_answer(s_m_solved);
+
+	cout<<(int)sudoku_matrix[0][0][0]<<endl;
+	cout<<(int)sudoku_matrix[1][1][1]<<endl;
 }
